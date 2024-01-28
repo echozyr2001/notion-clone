a63006b57a5aabe53c3476dbbb04b96f5783a8cd
echozyr2001 <echo.zyr.2001@gmail.com>
2024-01-28 15:45:39 +0800

    feat: navigation
    finish build navigation

---
+++ app/(main)/(routes)/documents/page.tsx
@@ -0,0 +1,3 @@
+export default function DocumentsPage() {
+  return <div>documents test</div>;
+}
---
+++ app/(main)/layout.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useConvexAuth } from "convex/react";
+import { redirect } from "next/navigation";
+
+import Spinner from "@/components/spinner";
+
+import Navigation from "./_components/navigation";
+
+export default function MainLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
+  if (isLoading) {
+    return (
+      <div className="h-full flex items-center justify-center">
+        <Spinner size="lg" />
+      </div>
+    );
+  }
+
+  if (!isAuthenticated) {
+    redirect("/");
+  }
+
+  return (
+    <div className="h-full flex dark:bg-[#1F1F1F]">
+      <Navigation />
+      <main className="flex h-full overflow-auto">{children}</main>
+    </div>
+  );
+}
